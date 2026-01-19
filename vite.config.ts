4c0d5ab3d8af42657888034fd439b00e3fdf491d
ejkim2933 <ejkim2933@gmail.com>
2026-01-19 20:13:01 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    // 빌드 중 에러가 나도 멈추지 않도록 설정 (안전장치)
-    chunkSizeWarningLimit: 1600,
+    outDir: 'dist',
   }
 })
