0378300b6590d6dbdb8dbc8e66653a97e4c91c19
ejkim2933 <ejkim2933@gmail.com>
2026-01-19 12:59:19 +0900

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -11,6 +11,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
