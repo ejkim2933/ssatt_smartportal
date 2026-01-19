b5fbb112ac880316e1621a7336b47e4320129886
ejkim2933 <ejkim2933@gmail.com>
2026-01-19 13:52:31 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 🔽 이 부분이 "하얀 화면"을 고쳐주는 핵심 치료제입니다!
+  // 🔽 여기가 핵심! 브라우저가 process나 global 단어를 봐도 당황하지 않게 만듭니다.
   define: {
-    'process.env': {}
+    'process.env': {},
+    'global': 'window',
   }
 })
