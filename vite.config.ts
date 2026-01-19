cac28c045c328c861b549fc37bf844576e9d7dbf
ejkim2933 <ejkim2933@gmail.com>
2026-01-19 17:13:35 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 🔽 여기가 핵심! 브라우저가 process나 global 단어를 봐도 당황하지 않게 만듭니다.
   define: {
     'process.env': {},
-    'global': 'window',
+    'global': 'window'
   }
 })
