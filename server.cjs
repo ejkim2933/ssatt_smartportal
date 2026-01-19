d8a2fae30432ff679cd257879b84d3981560ada6
ejkim2933 <ejkim2933@gmail.com>
2026-01-19 12:53:50 +0900

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-// 리액트가 빌드한 파일들(dist 폴더)을 손님에게 서빙합니다.
+
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// 어떤 주소로 들어오든 무조건 index.html을 보여줍니다 (SPA 방식)
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
